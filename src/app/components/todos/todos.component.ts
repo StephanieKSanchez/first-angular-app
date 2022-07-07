19e63d7f5b4ee973abe980f0bd59e1f48b4a4a26
Stephanie Sanchez <stephk006@gmail.com>
2022-07-06 22:50:01 -0400

    starting todo list - added an index, title, and delete button

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todos',
+  selector: 'app-todos', //this is how we use this component in oother components
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
