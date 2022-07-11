b392f536d1cd6e99bf856cadb1c13d959cf03e11
Stephanie Sanchez <stephk006@gmail.com>
2022-07-11 09:58:26 -0400

    create and call toggleDone method to strike through item when clicked

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -26,4 +26,11 @@ export class TodosComponent implements OnInit {
       },
     ];
   }
+  toggleDone(id: number) {
+    this.todos.map((value, index) => {
+      if (index == id) value.completed = !value.completed;
+
+      return value;
+    });
+  }
 }
